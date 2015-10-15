68f13168359828d8d01efaff2bf152c1cd01f161
Howard Cordray <hcordray@handy.com>
2015-10-15 10:08:53 -0400

    Initial Commit

---
+++ InfiniteScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfiniteScroll
+//
+//  Created by Howard Cordray on 10/15/15.
+//  Copyright © 2015 Howard Cordray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InfiniteScroll/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InfiniteScroll
+//
+//  Created by Howard Cordray on 10/15/15.
+//  Copyright © 2015 Howard Cordray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
